49fac3a462cc2b8d4a19c276a987c2eccc00d540
andrei-im <andreiimotroc@gmail.com>
2023-12-09 20:16:14 +0200

    added pagination

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask';
 import { useState } from "react";
 
+
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,7 +26,45 @@ function App() {
       day: "Feb 5th at 2:30pm",
       reminder: false,
     },
+    {
+      id: 4,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 5,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 6,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+    {
+      id: 7,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 8,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 9,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
   ])
+
+  const tasksPerPage = 5;
 
   // Add Task
   const addTask = (task) => {
@@ -43,11 +83,33 @@ function App() {
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
   }
 
+  // Paginate tasks
+  const indexOfLastTask = currentPage * tasksPerPage;
+  const indexOfFirstTask = indexOfLastTask - tasksPerPage;
+  const currentTasks = tasks.slice(indexOfFirstTask, indexOfLastTask);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+
   return (
-    <div className='container'>
-     <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-     {showAddTask && <AddTask onAdd={addTask}/>}
-     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {currentTasks.length > 0 ? (
+        <>
+          <Tasks tasks={currentTasks} onDelete={deleteTask} onToggle={toggleReminder} />
+          <div>
+            {Array.from({ length: Math.ceil(tasks.length / tasksPerPage) }, (_, index) => (
+              <button className="btn" style={{ backgroundColor: 'teal'}} key={index + 1} onClick={() => paginate(index + 1)}>
+                {index + 1}
+              </button>
+            ))}
+          </div>
+        </>
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   );
 }
